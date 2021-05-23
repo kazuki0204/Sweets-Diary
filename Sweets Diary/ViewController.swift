dcbf2a82769258d8fffcf5d5a65c7a219256aea6
Kazuki Marui <ka_ki0204@icloud.com>
2021-05-23 11:53:44 +0900

    Initial Commit

---
+++ Sweets Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sweets Diary
+//
+//  Created by 丸井一輝 on 2021/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
